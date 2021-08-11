0496f7db411377f12923a1fd67b7d15e5c0e2e39
Hiroshi MORISHIGE <hiroshi.morishige@gmail.com>
2021-08-11 16:29:25 +0900

    initial commit

---
+++ api/get-photos/index.ts
@@ -0,0 +1,39 @@
+import { APIGatewayProxyEventV2, Context, APIGatewayProxyStructuredResultV2 } from 'aws-lambda';
+import { S3 } from 'aws-sdk';
+
+const s3 = new S3();
+const bucketName = process.env.PHOTO_BUCKET_NAME!;
+
+async function generateUrl(object: S3.Object): Promise<{ filename: string; url: string }> {
+  const url = await s3.getSignedUrlPromise('getObject', {
+    Bucket: bucketName,
+    Key: object.Key!,
+    Expires: 24 * 60 * 60,
+  });
+  return {
+    filename: object.Key!,
+    url: url,
+  };
+}
+
+async function getPhotos(
+  event: APIGatewayProxyEventV2,
+  context: Context,
+): Promise<APIGatewayProxyStructuredResultV2> {
+  console.log('I got the bucket name and it is ' + bucketName);
+  try {
+    const { Contents: results } = await s3.listObjects({ Bucket: bucketName }).promise();
+    const photos = await Promise.all(results!.map((result) => generateUrl(result)));
+    return {
+      statusCode: 200,
+      body: JSON.stringify(photos),
+    };
+  } catch (error: any) {
+    return {
+      statusCode: 500,
+      body: error.message,
+    };
+  }
+}
+
+export { getPhotos };
--- test/simple-app.test.ts
+++ test/simple-app.test.ts
@@ -1,13 +1,13 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as SimpleApp from '../lib/simple-app-stack';
+import '@aws-cdk/assert/jest';
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new SimpleApp.SimpleAppStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test('Stack create a S3 bucket', () => {
+  // ARRANGE
+  const app = new cdk.App();
+  // ACT
+  const stack = new SimpleApp.SimpleAppStack(app, 'MyTestStack');
+  // ASSERT
+  expect(stack).toHaveResource('AWS::S3::Bucket');
 });
